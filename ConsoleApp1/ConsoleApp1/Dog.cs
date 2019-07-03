aa9f5503349fec9058620c59dd09df0980b06a99
Philip <Philip>
2019-07-03 15:26:44 -0500

    Program Compiles and runs

---
+++ ConsoleApp1/ConsoleApp1/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Dog
+    {
+        public Dog(string dogName)
+        {
+            Name = dogName;
+            Console.WriteLine("Instantiating Dog....");
+        }
+
+        public string Name { get; set; }
+        public int NumberOfLegs { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine("Ruff my name is " + Name + "!") ;
+        {
+
+        }
+        }
+    }
+}
---
+++ directorynameproject2/directorynameproject2/Questionaire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Questionaire
+{
+    public class Questionaire //the properties are here. these are the blueprint of your methods. cookie cutter, not the cookie
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal FavoriteNumber { get; set; }
+        public decimal NumberOfPets { get; set; }
+        //public List<string> NameOfPets { get; set; } = new List<string>();//this is now a list. was a string <failed attempt>
+        public string NameOfPets { get; set;  }
+    }
+}
+
+//more methods can be down here to get new methods (method within a method) see lemonade stand example for total revenue 
